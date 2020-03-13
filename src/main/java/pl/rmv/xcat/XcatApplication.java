8d0e7a1775a84f3fe5beb3e2163109bf2c0715bb
maxmayster <max.renkevych@gmail.com>
2020-03-13 17:13:07 +0100

    init

---
+++ src/main/java/pl/rmv/xcat/XcatApplication.java
@@ -0,0 +1,13 @@
+package pl.rmv.xcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcatApplication.class, args);
+	}
+
+}
